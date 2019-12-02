d4757353229cd063e2f43715832146e924a73cfb
Ben Gosney <bengosney@googlemail.com>
2019-12-02 08:44:31 +0000

    Day 2, Part 1

--- day2.dart
+++ day2.dart
@@ -2,20 +2,11 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
-int fuelReq(int weight) {
-  int req = (weight / 3).floor() - 2;
-
-  if (req > 0) {
-    return req + fuelReq(req);
-  } else {
-    return 0;
-  }
-}
 
 void main() async {
   var input = new List<int>();
 
-  String raw = await File('day2.test').readAsString();
+  String raw = await File('day2.input').readAsString();
 
   raw.split(",").forEach((String l) {
       input.add(int.parse(l));
@@ -24,32 +15,28 @@ void main() async {
   int opcode = 0;
   int pos = 0;
 
+  input[1] = 12;
+  input[2] = 2;
+
   while(opcode != 99) {
     opcode = input[pos];
-    print("opcode: $opcode, pos: $pos");
 
+    int a = input[pos + 1];
+    int b = input[pos + 2];
+    int o = input[pos + 3];
+    
     if (opcode == 1) {
-      input[pos + 3] = input[pos + 1] + input[pos + 2];
-      var o = pos + 3;
-      var a = input[pos + 1];
-      var b = input[pos + 2];
-      print("$o = $a + $b");
+      input[o] = input[a] + input[b];
     }
+    
     if (opcode == 2) {
-      input[pos + 3] = input[pos + 1] * input[pos + 2];
-      var o = pos + 3;
-      var a = input[pos + 1];
-      var b = input[pos + 2];
-      print("$o = $a * $b");
+      input[o] = input[a] * input[b];
     }
-    print(input);
 
     pos += 4;
-    if (pos >= input.length) {
-      pos -= (input.length - 1);
-    }
-    opcode = input[pos];  
+    
+    opcode = input[pos];
   }
 
-  print(input[0]);
+  print("Part 1: ${input[0]}");
 }
