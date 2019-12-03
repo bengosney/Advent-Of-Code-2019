5da6217213a3bf2baae418a3d4f1a62c9bf77c57
Ben Gosney <bengosney@googlemail.com>
2019-12-03 14:12:45 +0000

    Day 3, tidy and type safe

--- day3.dart
+++ day3.dart
@@ -4,9 +4,9 @@ import 'dart:convert';
 import 'dart:math';
 
 Map getLines(List ins) {
-  var DX = {'L': -1, 'R': 1, 'U': 0, 'D': 0};
-  var DY = {'L': 0, 'R': 0, 'U': 1, 'D': -1};
-  var ans = {};
+  Map DX = {'L': -1, 'R': 1, 'U': 0, 'D': 0};
+  Map DY = {'L': 0, 'R': 0, 'U': 1, 'D': -1};
+  Map ans = {};
   
   int x = 0;
   int y = 0;
@@ -34,39 +34,30 @@ void main() async {
   .transform(utf8.decoder)
   .transform(new LineSplitter())
   .forEach((String l) {
-      var cmds = new List<String>();
+      List cmds = new List<String>();
       l.split(",").forEach((String c) {
           cmds.add(c);
       });
       wires.add(cmds);
   });
 
-  var a = getLines(wires[0]);
-  var b = getLines(wires[1]);
+  Map a = getLines(wires[0]);
+  Map b = getLines(wires[1]);
 
-  var i = [];
-
-  var mh_dist = 99999999;
-  var sh_dist = 99999999;
+  var mh_dists = new List<int>();
+  var sh_dists = new List<int>();
   
   a.forEach((k, v) {      
       if (b.containsKey(k)) {
-        var parts = k.split('|');
+        List parts = k.split('|').map((String s) => int.parse(s).abs()).toList();
         
-        int x = int.parse(parts[0]).abs();
-        int y = int.parse(parts[1]).abs();
-        int mh = x + y;
-
-        if (mh_dist > mh) {
-          mh_dist = mh;
-        }
-
-        int dist = a[k] + b[k];
-        if (sh_dist > dist) {
-          sh_dist = dist;
-        }
+        mh_dists.add(parts[0] + parts[1]);
+        sh_dists.add(a[k] + b[k]);
       }
   });
+
+  int mh_dist = mh_dists.reduce(min);
+  int sh_dist = sh_dists.reduce(min);
 
   print("Part 1: ${mh_dist}");
   print("Part 2: ${sh_dist}");
