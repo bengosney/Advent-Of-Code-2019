6c94582462a5f06210486bc3d84188d1d2a6c472
Ben Gosney <bengosney@googlemail.com>
2019-12-03 09:00:41 +0000

    Day 3 still not working

--- day3.dart
+++ day3.dart
@@ -2,11 +2,34 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
+Map getLines(List ins) {
+  var DX = {'L': -1, 'R': 1, 'U': 0, 'D': 0};
+  var DY = {'L': 0, 'R': 0, 'U': 1, 'D': -1};
+  var ans = {};
+  
+
+  ins.forEach((d) {
+      int x = 0;
+      int y = 0;
+      int l = 0;
+      int dis = int.parse(d.substring(1));
+      String cmd = d[0];
+      
+      for (int i = 0; i < dis ; i++) {
+        x += DX[cmd];
+        y += DY[cmd];
+        l += 1;
+        ans["$x|$y"] = l;
+      }
+  });
+
+  return ans;
+}
 
 void main() async {
   var wires = new List<List<String>>();
   
-  await File('day3.input')
+  await File('day3.test')
   .openRead()
   .transform(utf8.decoder)
   .transform(new LineSplitter())
@@ -18,9 +41,22 @@ void main() async {
       wires.add(cmds);
   });
 
-  var grid = new List<List<String>>();
+  var a = getLines(wires[0]);
+  var b = getLines(wires[1]);
+
+  var i = [];
+
+  print(a);
+  print("");
+  print(b);
+  print("");
   
-  wires.forEach((w) {
-      print("Wire: ${w}");
+  a.forEach((k, v) {
+      print(k);
+      if (b.containsKey(k)) {
+        
+      }
   });
+
+  print(i);
 }
