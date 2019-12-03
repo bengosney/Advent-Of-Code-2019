f81826d87623442301bf0c22d66b933b2328be6e
Ben Gosney <bengosney@googlemail.com>
2019-12-03 13:58:32 +0000

    Day 3 parts 1 & 2 working

--- day3.dart
+++ day3.dart
@@ -1,17 +1,17 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
+import 'dart:math';
 
 Map getLines(List ins) {
   var DX = {'L': -1, 'R': 1, 'U': 0, 'D': 0};
   var DY = {'L': 0, 'R': 0, 'U': 1, 'D': -1};
   var ans = {};
   
-
+  int x = 0;
+  int y = 0;
+  int l = 0;
   ins.forEach((d) {
-      int x = 0;
-      int y = 0;
-      int l = 0;
       int dis = int.parse(d.substring(1));
       String cmd = d[0];
       
@@ -29,7 +29,7 @@ Map getLines(List ins) {
 void main() async {
   var wires = new List<List<String>>();
   
-  await File('day3.test')
+  await File('day3.input')
   .openRead()
   .transform(utf8.decoder)
   .transform(new LineSplitter())
@@ -46,17 +46,28 @@ void main() async {
 
   var i = [];
 
-  print(a);
-  print("");
-  print(b);
-  print("");
+  var mh_dist = 99999999;
+  var sh_dist = 99999999;
   
-  a.forEach((k, v) {
-      print(k);
+  a.forEach((k, v) {      
       if (b.containsKey(k)) {
+        var parts = k.split('|');
         
+        int x = int.parse(parts[0]).abs();
+        int y = int.parse(parts[1]).abs();
+        int mh = x + y;
+
+        if (mh_dist > mh) {
+          mh_dist = mh;
+        }
+
+        int dist = a[k] + b[k];
+        if (sh_dist > dist) {
+          sh_dist = dist;
+        }
       }
   });
 
-  print(i);
+  print("Part 1: ${mh_dist}");
+  print("Part 2: ${sh_dist}");
 }
