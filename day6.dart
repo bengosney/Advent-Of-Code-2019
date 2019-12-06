f94084c90793f5bfffa979d0e8a11cca9d5f83e2
Ben Gosney <bengosney@googlemail.com>
2019-12-06 09:01:08 +0000

    Day 6 part 1

--- day6.dart
+++ day6.dart
@@ -1,32 +1,34 @@
 import 'dart:io';
 import 'dart:convert';
 
-class object {
-  var parent;
-  List children;
-  String name;
-
-  
-}
 
 void main() async {
   var input = List<int>();
 
-  var lines = List();
+  var objects = {};
+
+  int sumLinks(String of, int level) {
+    int total = level;
+
+    if (objects.containsKey(of)) {
+      objects[of].forEach((i) => total += sumLinks(i, level + 1));
+    }
+
+    return total;
+  }
   
   await File('day6.input')
       .openRead()
       .transform(utf8.decoder)
       .transform(LineSplitter())
       .forEach((String l) {
-          var o = new object();
           List<String> parts = l.split(')');
-          o.name = parts[0];
-          o.parent = parts[1];
-
-          lines.add(o);
+          if (!objects.containsKey(parts[0])) {
+            objects[parts[0]] = [];
+          }
+          objects[parts[0]].add(parts[1]);
   });
 
-
-  print(lines);
+  int total = sumLinks('COM', 0);
+  print("Part 1: ${total}");
 }
